7d5e1880b39ca6fec555cb70cc5246f30c1c831c
hakunamatata <xymbtc@163.com>
2023-03-18 17:38:52 +0800

    first commit

---
+++ config/config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from '@umijs/max';
+import { routes } from './routes';
+export default defineConfig({
+  // 默认路由模式是“browser”，打包后会出现首页或其他页面找不到继而加载不出来的情况，所以需要改成hash模式。
+  hash: true,
+  history: {
+    // 配置 history 类型和配置项
+    type: 'hash', // 可选 browser、hash 和 memory
+  },
+  // 生产环境下需走相对路径
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : './', // 配置 webpack 的 publicPath。当打包的时候，webpack 会在静态文件路径前面添加 publicPath 的值
+  // 指定webpack包输出路径 防止与electron包路径重叠  再将build包 打包进electron
+  outputPath: './dist/render',
+
+  /* ----------------------------------------------------------------------------*/
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: '@umijs/max',
+  },
+  routes,
+  npmClient: 'pnpm',
+});
